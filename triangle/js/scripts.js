891ce3939bcc8f27ad1f8e545f3374f8036231ff
Kyle Derrick and Will Murphy <kderrick1988@gmail.com>
2016-01-19 15:49:13 -0800

    build user interface - second run

--- triangle/js/scripts.js
+++ triangle/js/scripts.js
@@ -16,7 +16,22 @@ var triangleType = function(length1, length2, length3) {
 
 };
 
-//
-// && ((length1 + length2) > length3) ||
-// && ((length1 + length3) > length2) ||
-// && ((length3 + length2) > length1))
+
+$(document).ready(function() {
+  $('form#triangle').submit(function(event) {
+    var length1 = parseInt($('input#length1').val());
+    var length2 = parseInt($('input#length2').val());
+    var length3 = parseInt($('input#length3').val());
+    var result = triangleType(length1, length2, length3);
+
+    $(".length1").text(length1);
+    $(".length2").text(length2);
+    $(".length3").text(length3);
+
+    $('.result').text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+
+});
